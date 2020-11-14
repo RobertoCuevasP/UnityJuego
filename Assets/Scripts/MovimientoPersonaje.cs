d255aeb8e55891ee9f3a4939b148d568c4d9d7c7
Roberto Cuevas Pinto <pablorobertocp1@gmail.com>
2020-11-13 22:31:15 -0400

    Físicas Creadas

--- Assets/Scripts/MovimientoPersonaje.cs
+++ Assets/Scripts/MovimientoPersonaje.cs
@@ -1,19 +1,29 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class MovimientoPersonaje : MonoBehaviour
 {
     public float speed = 0.00005f;
     public Vector2 direccion;
     private Vector2 movimiento;
+    private Rigidbody2D rigidbody;
     private Animator animator;
+    private bool camino;
+    private int direccionX;
+    private int direccionY;
+
 
     // Start is called before the first frame update
 
     void Awake()
     {
         animator = GetComponent<Animator>();
+        rigidbody = GetComponent<Rigidbody2D>();
+
     }
 
 
@@ -23,26 +33,35 @@ public class MovimientoPersonaje : MonoBehaviour
     }
 
     // Update is called once per frame
+    
+
     void Update()
     {
         float horizontalInput = Input.GetAxisRaw("Horizontal");
         float verticalInput = Input.GetAxisRaw("Vertical");
-        
-        movimiento = new Vector2(horizontalInput, verticalInput);
-        if(horizontalInput != 0)
+        //float attackInput = Input.GetAxisRaw("Jump");
+
+        if (horizontalInput != 0)
         {
-            if(horizontalInput > 0)
+            if (horizontalInput > 0)
             {
                 //animator.SetInteger("Movimiento", 4);
                 animator.SetTrigger("Derecha");
-            } else
+            }
+            else
             {
                 //animator.SetInteger("Movimiento", 3);
                 animator.SetTrigger("Izquierda");
             }
-        } else if(verticalInput != 0)
+            //animator.SetInteger("Moverse", (int)horizontalInput * 2);
+            //animator.SetBool("Quieto", false);
+            camino = true;
+            direccionX = (int) horizontalInput;
+            direccionY = 0;
+        }
+        else if (verticalInput != 0)
         {
-            if(verticalInput > 0)
+            if (verticalInput > 0)
             {
                 //animator.SetInteger("Movimiento", 1);
                 animator.SetTrigger("Arriba");
@@ -52,8 +71,37 @@ public class MovimientoPersonaje : MonoBehaviour
                 //animator.SetInteger("Movimiento", 2);
                 animator.SetTrigger("Abajo");
             }
+            //animator.SetInteger("Moverse", (int)verticalInput);
+            //animator.SetBool("Quieto", false);
+            camino = false;
+            direccionX = 0;
+            direccionY = (int)verticalInput;
         }
-        transform.Translate(movimiento); 
+
+        movimiento = new Vector2(horizontalInput, verticalInput);
+
+        
+    }
+
+    void FixedUpdate()
+    {
+        float horizontalVelocity = movimiento.normalized.x * speed;
+        float verticalVelocity = movimiento.normalized.y * speed;
+        if (camino)
+        {
+            rigidbody.velocity = new Vector2(horizontalVelocity, rigidbody.velocity.y);
+        }
+        else
+        {
+            rigidbody.velocity = new Vector2(rigidbody.velocity.x, verticalVelocity);
+        }
+
+    }
+
+
+    void LateUpdate()
+    {
+        //animator.SetBool("Quieto", movimiento == Vector2.zero);
     }
 
 }
